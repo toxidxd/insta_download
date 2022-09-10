cc0885a16ca3e4134aa3399a313a9729d9c2a520
toxidxd <toxid13@yandex.ru>
2022-09-10 14:22:42 +0300

    some fixes

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 import instaloader
-
+import random
 
 proxies = {
    'https': 'https://IP:port',
     }
 
-L = instaloader.Instaloader()
+L = instaloader.Instaloader(request_timeout=random.randint(300, 900))
 
 
 # L.login(USER, PASSW) # login for private account
 ins_prof = input("Input instagram profile name: ")
 
-L.download_profile(ins_prof, profile_pic_only=True)
+# L.download_profile(ins_prof, profile_pic_only=True)
+L.download_profile(ins_prof)
 
